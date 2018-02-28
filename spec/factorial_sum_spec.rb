a8f6da5c1299bd6df9270a5221dcb1a5f9f0e17d
Thomas Dorrian <thomasdrrn93@gmail.com>
2018-02-28 07:06:24 -0500

    create factorial specs

--- spec/factorial_sum_spec.rb
+++ spec/factorial_sum_spec.rb
@@ -2,9 +2,15 @@ require 'rspec'
 require 'factorial_sum'
 
 describe 'factorial' do
+  it 'returns the correct value if n is 0' do
+    expect(factorial(0)).to eq(1)
+  end
 
+  it 'returns the correct value if n is 10' do
+    expect(factorial(10)).to eq(3628800) 
+  end
 end
 
 describe 'factoriral_digit_sum' do
 
-end 
+end
