96daa029180ca48b94d15b8c32056cb7c8c4c000
Thomas Dorrian <thomasdrrn93@gmail.com>
2018-02-28 07:11:20 -0500

    specs for main method

--- spec/factorial_sum_spec.rb
+++ spec/factorial_sum_spec.rb
@@ -7,10 +7,20 @@ describe 'factorial' do
   end
 
   it 'returns the correct value if n is 10' do
-    expect(factorial(10)).to eq(3628800) 
+    expect(factorial(10)).to eq(3628800)
   end
 end
 
 describe 'factoriral_digit_sum' do
+  it 'returns the correct value if num is 0' do
+    expect(factoriral_digit_sum(0)).to eq(1)
+  end
 
+  it 'returns the correct value if num is 10' do
+    expect(factoriral_digit_sum(10)).to eq(27)
+  end
+
+  it 'returns the correct value if num is 100' do
+    expect(factoriral_digit_sum(100)).to eq(648)
+  end
 end
