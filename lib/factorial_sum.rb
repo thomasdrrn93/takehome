61f1913681bc07f21484810c64a44e46d329a471
Thomas Dorrian <thomasdrrn93@gmail.com>
2018-02-28 07:18:36 -0500

    create error handling

--- lib/factorial_sum.rb
+++ lib/factorial_sum.rb
@@ -1,9 +1,11 @@
 def factoriral_digit_sum(num)
+  raise ArgumentError, 'Argument is not an integer' unless num.is_a? Integer
   fact = factorial(num)
   fact.to_s.chars.map(&:to_i).reduce(:+)
 end
 
 def factorial(n)
+  raise ArgumentError, 'Argument is not a positive integer' unless n > -1
   return 1 if n == 0
   n * factorial(n - 1)
 end
--- spec/factorial_sum_spec.rb
+++ spec/factorial_sum_spec.rb
@@ -9,6 +9,10 @@ describe 'factorial' do
   it 'returns the correct value if n is 10' do
     expect(factorial(10)).to eq(3628800)
   end
+
+  it 'should raise ArgumentError for arguments that are negative integers' do
+    expect { factorial(-1) }.to raise_error(ArgumentError)
+  end
 end
 
 describe 'factoriral_digit_sum' do
@@ -22,5 +26,13 @@ describe 'factoriral_digit_sum' do
 
   it 'returns the correct value if num is 100' do
     expect(factoriral_digit_sum(100)).to eq(648)
+  end
+
+  it 'should raise ArgumentError for arguments that are not integers' do
+    expect { factoriral_digit_sum([1, 2, 3]) }.to raise_error(ArgumentError)
+  end
+
+  it 'should raise ArgumentError for arguments that are negative integers' do
+    expect { factoriral_digit_sum(-1) }.to raise_error(ArgumentError)
   end
 end
